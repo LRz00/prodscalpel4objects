f3f7bd3908c638d993732979f8c3740589b9da4a
giovaneneves7 <autornekoyasha@gmail.com>
2025-02-04 19:37:50 -0500

    Criando classe GPAlgorthm

---
+++ src/main/java/com/ifba/prodscalpel4objects/adapter/GPAlgorithm.java
@@ -0,0 +1,16 @@
+/**
+* GP Algorithm para adaptação de código.
+*
+* @author Giovane Neves
+*/
+public class GPAlgorithm {
+
+   /**
+   * Executa o GP Algorithm.
+   * 
+   */
+   public void run(){
+     // TODO: Adiciona lógica do GP Algorithm 
+   }
+
+}
