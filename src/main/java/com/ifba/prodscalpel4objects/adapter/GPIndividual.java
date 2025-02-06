4de1735d72804e304055f8fa76977974a331042b
giovaneneves7 <autornekoyasha@gmail.com>
2025-02-06 16:14:49 -0500

    Criando classe GPIndividual

---
+++ src/main/java/com/ifba/prodscalpel4objects/adapter/GPIndividual.java
@@ -0,0 +1,19 @@
+package com.ifba.prodscalpel4objects.adapter;
+
+/**
+ * Classe que representa um individuo.
+ * @author Giovane Neves
+ */
+public class GPIndividual {
+
+    /**
+     * O ID do individuo
+     */
+    int id;
+
+    /**
+     * A fitness do individuo
+     */
+    double fitness;
+
+}
