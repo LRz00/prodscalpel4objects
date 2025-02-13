ab3fb1ac263d016b93e0870f6bd60a7ad0cd7751
giovaneneves7 <autornekoyasha@gmail.com>
2025-02-13 18:05:54 -0300

    Adicionando novos atributos

--- src/main/java/com/ifba/prodscalpel4objects/adapter/GPIndividual.java
+++ src/main/java/com/ifba/prodscalpel4objects/adapter/GPIndividual.java
@@ -1,5 +1,7 @@
 package com.ifba.prodscalpel4objects.adapter;
 
+import java.util.List;
+
 /**
  * Classe que representa um individuo.
  * @author Giovane Neves
@@ -7,13 +9,48 @@ package com.ifba.prodscalpel4objects.adapter;
 public class GPIndividual {
 
     /**
-     * O ID do individuo
+     * Individual's id
      */
-    int id;
+    private int id;
 
     /**
-     * A fitness do individuo
+     * The individual's fitness
      */
-    double fitness;
+    private double fitness;
+    private GPIndividual next;
+    private List<Integer> selectedLOCs;
 
+    // Boilerplate code
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+
+    public GPIndividual getNext() {
+        return next;
+    }
+
+    public void setNext(GPIndividual next) {
+        this.next = next;
+    }
+
+    public List<Integer> getSelectedLOCs() {
+        return selectedLOCs;
+    }
+
+    public void setSelectedLOCs(List<Integer> selectedLOCs) {
+        this.selectedLOCs = selectedLOCs;
+    }
 }
