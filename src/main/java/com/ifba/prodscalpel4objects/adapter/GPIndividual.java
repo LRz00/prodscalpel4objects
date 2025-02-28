8bcfe33a5d0d0a673d1f7653f5ea06c5fa3ba37c
giovaneneves7 <autornekoyasha@gmail.com>
2025-02-28 18:49:10 -0300

    Adicionando atributo 'abstractMappings' à classe GPIndividual

--- src/main/java/com/ifba/prodscalpel4objects/adapter/GPIndividual.java
+++ src/main/java/com/ifba/prodscalpel4objects/adapter/GPIndividual.java
@@ -1,47 +1,65 @@
+// =======================> START - PACKAGE <======================= //
 package com.ifba.prodscalpel4objects.adapter;
+// =======================> END - PACKAGE <======================= //
 
+// =======================> START - IMPORTS <======================= //
 import java.util.ArrayList;
 import java.util.List;
+// =======================> END - IMPORTS <======================= //
 
 /**
- * Classe que representa um individuo.
+ * Class that represents an Individual.
+ *
  * @author Giovane Neves
  */
+// =======================> START - CLASS <======================= //
 public class GPIndividual {
 
-    /**
-     * Individual's id
-     */
-    private int id;
+    // =======================> START - ATTRIBUTES <======================= //
+    private int id; // Individual's unique indentifier
+    private double fitness; // Individual's fitness
+    private GPIndividual next; // The next Individual
+    private List<Integer> selectedLOCs; // The selected lines of code
+    private List<Mapping> abstractMappings; // The Abstract Symbol Tree
 
-    /**
-     * The individual's fitness
-     */
-    private double fitness;
-    private GPIndividual next;
-    private List<Integer> selectedLOCs;
+    // ========================================================================= //
+    // TODO: Estudar essas 4 variáveis
+    //List<String> mappedDeclarations;
+    //DeclarationSymbolTable declarationSymbolTable;
+    //List<MappingCandidate> candidateMappings
+    //List<MappingCandidate> multiPossibleMappings
+    // ========================================================================= //
 
+    // =======================> END - ATTRIBUTES <======================= //
+
+
+    // =======================> START - CONSTRUCTOR <======================= //
     public GPIndividual() {
         this.selectedLOCs = new ArrayList<>();
     }
+    // =======================> END - CONSTRUCTOR <======================= //
 
-    // Boilerplate code
+    // ========================================================================== //
+    // ========================================================================== //
+    // =======================> START - BOILERPLATE CODE <======================= //
+    // ========================================================================== //
+    // ========================================================================== //
 
     public int getId() {
         return id;
-    }
+    } // getId
 
     public void setId(int id) {
         this.id = id;
-    }
+    } // setId
 
     public double getFitness() {
         return fitness;
-    }
+    } // getFitness
 
     public void setFitness(double fitness) {
         this.fitness = fitness;
-    }
+    } // setFitness
 
     public GPIndividual getNext() {
         return next;
@@ -49,13 +67,24 @@ public class GPIndividual {
 
     public void setNext(GPIndividual next) {
         this.next = next;
-    }
+    } // getNext
 
     public List<Integer> getSelectedLOCs() {
         return selectedLOCs;
-    }
+    } // getSelectedLOCs
 
     public void setSelectedLOCs(List<Integer> selectedLOCs) {
         this.selectedLOCs = selectedLOCs;
-    }
+    } // setSelectedLOCs
+
+    public List<Mapping> getAbstractMappings() {
+        return abstractMappings;
+    } // getAbstractMappings
+
+    public void setAbstractMappings(List<Mapping> abstractMappings) {
+        this.abstractMappings = abstractMappings;
+    } // setAbstractMappings
+
+    // =======================> END - BOILERPLATE CODE <======================= //
 }
+// =======================> END - CLASS <======================= //
